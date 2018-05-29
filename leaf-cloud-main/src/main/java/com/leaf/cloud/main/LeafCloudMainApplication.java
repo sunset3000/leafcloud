dd1fa8d9f325a2b6b9ad3f99c5e847f2d8927b40
Tian Fan <tian.fan@citrix.com>
2018-05-29 13:49:19 +0800

    init with main project

---
+++ leaf-cloud-main/src/main/java/com/leaf/cloud/main/LeafCloudMainApplication.java
@@ -0,0 +1,12 @@
+package com.leaf.cloud.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafCloudMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafCloudMainApplication.class, args);
+	}
+}
